7c6ca7705346a1e28f2b5c770ca2dd0c4e0126ab
Daniel Haro Contreras <daniharopadul@gmail.com>
2022-10-29 18:01:59 +0200

    Change colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,13 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className="button button--secondary button--lg shadow--low"
             to="/docs/intro"
           >
             Tutorial de Mordente - 5min ⏱️
           </Link>
           <Link
-            className="button button--info button--lg"
+            className="button button--primary button--lg shadow--low"
             href="https://t.me/mordente_bot"
           >
             ¡Pruébalo ahora!
